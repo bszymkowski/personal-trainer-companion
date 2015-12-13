aad54e0d312e05de56ccbb0ba3ef060369a8042a
Szymkowski <wuj.zenon@gmail.com>
2015-12-13 19:26:47 +0100

    One more test for AddPaymentDialog
    I think I'm in love with Spock

--- app/src/test/groovy/com/szymkowski/personaltrainercompanion/payments/addpayment/AddPaymentDialogTest.groovy
+++ app/src/test/groovy/com/szymkowski/personaltrainercompanion/payments/addpayment/AddPaymentDialogTest.groovy
@@ -25,4 +25,18 @@ class AddPaymentDialogTest extends GradleRoboSpecification {
             1* dialogCallback.addPayment(_ as PaymentDTO)
     }
 
+    def 'should not call callback payment adding cancelled'() {
+        given:
+            def dialogCallback = Mock(AddPaymentDialogCallback)
+            AddPaymentDialog dialog = new AddPaymentDialog(RuntimeEnvironment.application.getApplicationContext(), dialogCallback)
+        when:
+            dialog.show()
+        then:
+            ShadowDialog.latestDialog as AddPaymentDialog
+        when:
+            dialog.findViewById(R.id.add_payment_dialog_button_cancel).performClick()
+        then:
+            0* dialogCallback.addPayment(_ as PaymentDTO)
+    }
+
 }
